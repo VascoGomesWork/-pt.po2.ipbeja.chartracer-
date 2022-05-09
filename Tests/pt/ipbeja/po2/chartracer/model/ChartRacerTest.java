0d4ada41334c25424a978e0eeb5d861cc9318ce3
VascoGomesWork <50702151+VascoGomesWork@users.noreply.github.com>
2022-05-09 20:25:45 +0100

    First Test Created

--- Tests/pt/ipbeja/po2/chartracer/model/ChartRacerTest.java
+++ Tests/pt/ipbeja/po2/chartracer/model/ChartRacerTest.java
@@ -1,5 +1,16 @@
 package pt.ipbeja.po2.chartracer.model;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
@@ -8,6 +19,30 @@ import static org.junit.jupiter.api.Assertions.*;
  */
 class ChartRacerTest {
 
-    
+    ChartRacer chartRacer;
+    String fileName = "src/pt/ipbeja/po2/chartracer/model/cities.txt";
 
+    @BeforeEach
+    void setUp() {
+        this.chartRacer = new ChartRacer();
+    }
+
+    @Test
+    void teste1() {
+        //Ler um ficheiro de dados e verificar que está bem lido (os dados lidos são os esperados);
+        List<String> fileData = null;
+        try {
+
+            fileData = Files.readAllLines(Paths.get(fileName));
+            assertEquals(fileData, chartRacer.readFile(fileName));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    void teste2() {
+
+    }
 }
--- src/pt/ipbeja/po2/chartracer/model/ChartRacer.java
+++ src/pt/ipbeja/po2/chartracer/model/ChartRacer.java
@@ -1,5 +1,7 @@
 package pt.ipbeja.po2.chartracer.model;
 
+import java.util.List;
+
 /**
  * @author Vasco Gomes 19921
  * @date 09/05/2022
@@ -7,4 +9,7 @@ package pt.ipbeja.po2.chartracer.model;
 public class ChartRacer {
 
 
+    public List<String> readFile(String fileName) {
+        return null;
+    }
 }
