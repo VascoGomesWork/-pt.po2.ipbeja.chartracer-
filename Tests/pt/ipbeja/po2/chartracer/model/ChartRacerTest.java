0bb3486dd57ef633702f3f662f7e1ed822337822
VascoGomesWork <50702151+VascoGomesWork@users.noreply.github.com>
2022-05-09 20:23:46 +0100

    Tests Class Created

---
+++ Tests/pt/ipbeja/po2/chartracer/model/ChartRacerTest.java
@@ -0,0 +1,13 @@
+package pt.ipbeja.po2.chartracer.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author Vasco Gomes 19921
+ * @date 09/05/2022
+ */
+class ChartRacerTest {
+
+    
+
+}
