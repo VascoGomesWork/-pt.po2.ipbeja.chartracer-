e4afa63a910f1ecf44b312d791e1319fc237cdad
VascoGomesWork <50702151+VascoGomesWork@users.noreply.github.com>
2022-05-09 20:28:01 +0100

    First Test Executing

--- src/pt/ipbeja/po2/chartracer/model/ChartRacer.java
+++ src/pt/ipbeja/po2/chartracer/model/ChartRacer.java
@@ -1,5 +1,11 @@
 package pt.ipbeja.po2.chartracer.model;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -9,7 +15,21 @@ import java.util.List;
 public class ChartRacer {
 
 
+    /**
+     * Resume : Method to Read Cities Data from the File
+     * @param fileName
+     * @return
+     */
     public List<String> readFile(String fileName) {
+        try {
+            return Files.readAllLines(Paths.get(fileName));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public List<String> readFile2(String fileName){
         return null;
     }
 }
